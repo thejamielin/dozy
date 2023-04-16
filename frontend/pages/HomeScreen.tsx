660f4e75debff7d07a11e9e3bdb93c0ed3e3f7f5
Jamie Lin <linmjam@amazon.com>
2023-04-16 06:41:04 -0400

    reformat home page

--- frontend/pages/HomeScreen.tsx
+++ frontend/pages/HomeScreen.tsx
@@ -1,33 +1,33 @@
-import { Button, Badge } from 'react-native-paper';
-import { View, Text } from 'react-native';
-import Pet from '../components/Pet'; 
-import AwakeSwitch from '../components/AwakeSwitch';
-
+import { Button, Badge } from "react-native-paper";
+import { View, Text } from "react-native";
+import Pet from "../components/Pet";
+import AwakeSwitch from "../components/AwakeSwitch";
 
 const HomeScreen: React.FC = ({ navigation }: any) => {
-    const streak = 0; 
-    const duration = 2; 
-    return (
-      <View style={{alignContent:'center'}}>
-        {/* <Button
-          mode="contained"
-          children="Awake"
-        /> */}
-        <AwakeSwitch /> 
+  const streak = 0;
+  const duration = 2;
+  return (
+    <View
+      style={{
+        alignContent: "center",
+      }}
+    >
+      <AwakeSwitch />
+      <Pet />
+      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <View>
+          <Badge style={{ alignSelf: "center" }}> Streak: {streak} </Badge>
+          <Badge style={{ alignSelf: "center" }}> Duration: {duration} </Badge>
+        </View>
         <Button
+          icon=""
           mode="contained"
           children="Tips and Tricks for Better Sleep"
-          onPress={() =>
-            navigation.navigate('Info')
-          }
+          onPress={() => navigation.navigate("Info")}
         />
-        <Pet/>
-        <Badge style={{alignSelf:'center'}} > Streak: {streak} </Badge> 
-        <Badge style={{alignSelf:'center'}}> Duration: {duration} </Badge> 
-      
       </View>
-    );
-  };
-
+    </View>
+  );
+};
 
 export default HomeScreen;
