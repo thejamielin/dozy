a42c7d4b915501cac59247cecbdd87a107344bd7
Jamie Lin <linmjam@amazon.com>
2023-04-15 21:46:08 -0400

    organize screens and components into pages and components folders

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,42 +1,23 @@
-import { Button } from 'react-native-paper';
-import { Text, View } from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SetupScreen from './pages/SetupScreen';
+import HomeScreen from './pages/HomeScreen';
+import InfoScreen from './pages/InfoScreen';
 const Stack = createNativeStackNavigator();
 
-const HomeScreen: React.FC = ({ navigation }: any) => {
-  return (
-    <View>
-      <Button
-        mode="contained"
-        children="Awake"
-        onPress={() =>
-          navigation.navigate('Info')
-        }
-      />
-      <Button
-        mode="contained"
-        children="Tips and Tricks for Better Sleep"
-        onPress={() =>
-          navigation.navigate('Info')
-        }
-      />
-    </View>
-  );
-};
 
-const InfoScreen: React.FC = ({navigation, route}: any) => {
-  return <Text>
-    {sleepTips}
-  </Text>;
-};
 
 export default function App() {
   
   return (
     <NavigationContainer>
       <Stack.Navigator>
+      <Stack.Screen
+          name="Setup"
+          component={SetupScreen}
+          options={{title: 'Welcome to Dozy!'}}
+        />
         <Stack.Screen
           name="Home"
           component={HomeScreen}
@@ -47,5 +28,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const sleepTips: String = "1. Stick to a sleep schedule. \n2. Exercise is great, but not too late in the";
---
+++ frontend/components/Pet.tsx
@@ -0,0 +1,9 @@
+import { Text, View, Image } from 'react-native';
+
+const Pet: React.FC = () => {
+    return <Image
+          source={require('../assets/icon.png')}
+      />;
+}
+
+export default Pet;
