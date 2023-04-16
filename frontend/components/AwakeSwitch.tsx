e70eb674e75e2191e7546f2a4cbee42ab47878d2
Jamie Lin <linmjam@amazon.com>
2023-04-16 06:01:40 -0400

    add logic for awake switch connection to backend, some id reading issues

--- frontend/components/AwakeSwitch.tsx
+++ frontend/components/AwakeSwitch.tsx
@@ -1,27 +1,52 @@
 // CODE FROM: https://callstack.github.io/react-native-paper/docs/components/Switch/
 
-import * as React from 'react';
-import { Switch } from 'react-native-paper';
-import {View, Text} from 'react-native'; 
+import * as React from "react";
+import { Switch } from "react-native-paper";
+import { View, Text } from "react-native";
+import { useMutation } from "../convex/_generated/react";
 
 const AwakeSwitch = () => {
+  const sendSleep = useMutation("sendSleep");
+  const modSleep = useMutation("modSleep");
   const [isSwitchOn, setIsSwitchOn] = React.useState(false);
 
-  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+  const onToggleSwitch = () => {
+    handleStartOrEndSleep();
+    setIsSwitchOn(!isSwitchOn);
+  };
 
-  var status = ""; 
+  var status = "";
 
-  if (isSwitchOn){ 
-    status = "Sleeping"; 
+  if (isSwitchOn) {
+    status = "Sleeping";
+  } else {
+    status = "Awake";
   }
-  else { 
-    status = "Awake"; 
-  }
+
+  const handleStartOrEndSleep = () => {
+    let userId: string = "1"; // TODO this is hardcoded
+    let now: Date = new Date();
+    if (isSwitchOn) {
+      startSleep();
+    } else {
+      endSleep();
+    }
+
+    function endSleep() {
+      let endTime: string = now.toISOString();
+      modSleep({ userId, endTime });
+    }
+
+    function startSleep() {
+      let startTime: string = now.toISOString();
+      sendSleep({ userId, startTime });
+    }
+  };
 
   return (
-    <View style={{alignSelf:'center'}}>
-        <Text> {status} </Text>
-        <Switch value={isSwitchOn} onValueChange={onToggleSwitch} style={{size:20}}/>
+    <View style={{ alignSelf: "center" }}>
+      <Text> {status} </Text>
+      <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
     </View>
   );
 };
